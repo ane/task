79c709aa8137a7538b79cdc662084f2743e6ef4e
Antoine Kalmbach <ane@iki.fi>
2017-11-20 11:50:05 +0200

    Fix the API to use the protocol

--- src/main/clj/task/core.clj
+++ src/main/clj/task/core.clj
@@ -1,15 +1,58 @@
 (ns task.core
   (:refer-clojure :exclude [for sequence] :as core)
-  (:import [java.util.concurrent CompletableFuture ExecutionException ForkJoinPool TimeoutException TimeUnit]))
+  (:import [java.util.concurrent CompletableFuture Executor ForkJoinPool TimeoutException TimeUnit]))
+
+; boring Java FunctionalInterface crap
+(defn ^:no-doc fn->Supplier
+  [func]
+  (reify java.util.function.Supplier
+    (get [this] (func))))
+
+(defn ^:no-doc fn->Function
+  [func]
+  (reify java.util.function.Function
+    (apply [this t] (func t))
+    (andThen [this after]
+      (fn->Function #(.apply after (.apply this %1))))
+    (compose [this before]
+      (fn->Function #(.apply this (.apply before %1))))))
 
 (defprotocol Task
-  (task->future [this] "convert self into CompletableFuture"))
-
+  (task->future [this] "Convert this task into a CompletableFuture.")
+  (done? [this] "Has the task completed in any fashion?")
+  (cancel [this] "Attempt to cancel the future, returns true if it cancelled.")
+  (cancelled? [this] "Was the task cancelled?")
+  (complete! [this value] "Complete the task with some value if it hasn't completed already.")
+  (failed? [this] "Did the task complete with an exception of any kind?")
+  (failure [this] "If the task has completed exceptionally, get the Throwable that produced the exceptional completion.")
+  (else [this value] "Get the value of the task if it's complete. Otherwise return `value`. This is non-blocking.")
+  (fail! [this throwable] "Force the task to fail with `throwable` whether or not already completed.")
+  (force! [this value] "Force the task to return `value` whether or not already completed.")
+  (recover [this f]
+    "Recover possible failures in `task`. Returns a new task. This
+  task evaluates to two possible values. If the task completes with an
+  exception, this exception is passed to `func`, and the task
+  evaluates to its result. If the task completes normally, the task
+  will evaluate to that result."))
+  
 (defn future->task
+  "Convert a CompletableFuture into a Task."
   [^CompletableFuture fut]
   (reify
     Task
-    (task->future [_] fut)
+    (task->future [_]               fut)
+    (done?        [this]            (.isDone fut))
+    (cancel       [this]            (.cancel fut true))
+    (cancelled?   [this]            (.isCancelled fut))
+    (complete!    [this value]      (.complete fut value))
+    (force!       [this value]      (.obtrudeValue fut value))
+    (recover      [this f]          (future->task (.exceptionally fut (fn->Function f))))
+    (else         [this value]      (.getNow fut value))
+    (failed?      [this]            (.isCompletedExceptionally fut))
+    (failure      [this]            (when (failed? this)
+                                      (let [fail (try @this (catch Exception e e))]
+                                        (or (.getCause fail) fail))))
+    (fail!        [this throwable]      (.obtrudeException fut throwable))
 
     clojure.lang.IBlockingDeref
     (deref [_ timeout-ms timeout-val]
@@ -25,23 +68,10 @@
     (cancel [_ interrupt] (.cancel fut interrupt))))
 
 
-(defn supplier
-  [func]
-  (reify java.util.function.Supplier
-    (get [this] (func))))
-
-(defn function
-  [func]
-  (reify java.util.function.Function
-    (apply [this t] (func t))
-    (andThen [this after]
-      (function #(.apply after (.apply this %1))))
-    (compose [this before]
-      (function #(.apply this (.apply before %1))))))
-
 (defn fn->future
+  "Convert any function into a CompletableFuture."
   [func & [executor]]
-  (CompletableFuture/supplyAsync (supplier func) (or executor (ForkJoinPool/commonPool))))
+  (CompletableFuture/supplyAsync (fn->Supplier func) (or executor (ForkJoinPool/commonPool))))
 
 (defmacro run
   "Execute `body` inside the default [ForkJoinPool](https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/ForkJoinPool.html)
@@ -56,11 +86,6 @@
   "Create a task that completes immediately with `value`."
   [value]
   (future->task (CompletableFuture/completedFuture value)))
-
-(defn done?
-  "Has the task completed in any fashion?"
-  [task]
-  (.isDone (task->future task)))
 
 (defn- ensure-task
   [obj]
@@ -77,7 +102,9 @@
           (now (.get obj))
           (run (.get obj)))))
 
-(def common-pool (ForkJoinPool/commonPool))
+(def common-pool
+  "The default executor, [ForkJoinPool](https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/ForkJoinPool.html)."
+  (ForkJoinPool/commonPool))
 
 (defn then
   "Apply a function to the result of `task`. Returns a new task.
@@ -90,8 +117,8 @@
   that [ExecutorService](https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/ForkJoinPool.html)."
   ([func] (fn [task] (then func task)))
   ([func task] (then func task common-pool))
-  ([func task executor]
-   (future->task (.thenApplyAsync ((comp task->future ensure-task) task) (function func)
+  ([func task ^Executor executor]
+   (future->task (.thenApplyAsync ((comp task->future ensure-task) task) (fn->Function func)
                                   (or executor common-pool)))))
 
 (defn compose
@@ -109,15 +136,10 @@
   otherwise result in a task inside a task. Given an executor, use that."
   ([func] (fn [task] (compose func task common-pool)))
   ([func task] (compose func task common-pool))
-  ([func task executor]
+  ([func task ^Executor executor]
    (future->task (.thenComposeAsync ((comp task->future ensure-task) task)
-                                    (function (comp task->future ensure-task func))
+                                    (fn->Function (comp task->future ensure-task func))
                                     (or executor common-pool)))))
-
-(defn cancel
-  "Attempt to cancel `task`."
-  [task]
-  (future-cancel task))
 
 (defmacro for
   "Chain multiple tasks together. Returns a new task that evaluates when all the
@@ -131,7 +153,7 @@
   [executor bindings & body]
   `(for* ~executor ~bindings ~@body))
 
-(defmacro for*
+(defmacro ^:no-doc for*
   [executor bindings & body]
   (let [groups# (if (<= 2 (count bindings))
                   (partition 2 bindings)
@@ -148,10 +170,6 @@
                 ~executor)
       `(then (fn [~sym#] ~@body) ~fut# ~executor))))
 
-(defn cancelled?
-  "Return true if `task` was cancelled."
-  [task]
-  (future-cancelled? task))
 
 
 (defn void
@@ -170,52 +188,9 @@
           (now [])
           tasks))
 
-(defn complete!
-  "Complete the task with some value if it hasn't completed already."
-  [task value]
-  (.complete (task->future task) value))
-
 (defn failed
   "Create a new task that fails with `t`."
   [^java.lang.Throwable t]
   (let [task (void)]
     (.completeExceptionally (task->future task) t)
     task))
-
-(defn failed?
-  "Did the task complete with an exception of any kind?"
-  [task]
-  (.isCompletedExceptionally (task->future task)))
-
-(defn failure
-  "Get the exception with which the task completed exceptionally, if any."
-  [task]
-  (when (failed? task)
-    (let [fail (try @task (catch Exception e e))]
-      (or (.getCause fail) fail))))
-
-(defn else
-  "Get the value of `task` if it's complete, otherwise return
-  `value`."
-  [task value] (.getNow (task->future task) value))
-
-(defn fail!
-  "Force the value of task to fail with `t` whether or not already completed."
-  [task ^java.lang.Throwable t]
-  (.obtrudeException (task->future task) t))
-
-(defn force!
-  "Force the value of task to return `value` whether or not already completed."
-  [task value]
-  (.obtrudeValue (task->future task) value))
-
-(defn recover
-  "Recover possible failures in `task`. Returns a new task. This task evaluates to
-  two possible values:
-
-  * If the task completes with an exception, this exception is passed to `func`, and the task
-  evaluates to its result.
-  * If the task completes normally, the task will evaluate to that result."
-  [task func]
-  (let [jf (task->future task)]
-    (future->task (.exceptionally jf (function func)))))
--- src/test/clj/task/core_test.clj
+++ src/test/clj/task/core_test.clj
@@ -1,101 +1,100 @@
 (ns task.core-test
   (:require [clojure.test :refer :all]
-            [task.core :refer :all])
+            [task.core :as task])
   (:import java.util.concurrent.ExecutionException))
 
 ; testing I didn't screw up the Java functional crap
 (deftest supplier-tests
   (testing "supplier works"
-    (is (= 1 (.get (supplier (fn [] 1)))))))
+    (is (= 1 (.get (task/fn->Supplier (fn [] 1)))))))
 
 (deftest function-tests
   (testing "application"
-    (is (= 16 (.apply (function #(* 2 %1)) 8)))))
+    (is (= 16 (.apply (task/fn->Function #(* 2 %1)) 8)))))
 
 (deftest composition 
   (testing "composition"
-    (is (= 2 (let [fst (function #(* 1 %))
-                   snd (function #(* -1 %))]
+    (is (= 2 (let [fst (task/fn->Function #(* 1 %))
+                   snd (task/fn->Function #(* -1 %))]
                (.apply (.compose fst snd) -2))))))
 
 (deftest chaining
   (testing "chaining"
-   (is (= -2 (let [fst (function #(* 1 %1))
-                   snd (function #(* -1 %1))]
+   (is (= -2 (let [fst (task/fn->Function #(* 1 %1))
+                   snd (task/fn->Function #(* -1 %1))]
               (.apply (.andThen fst snd) 2))))))
 
 ;; basic stuff
 
 (deftest promotion
   (testing "values can be promoted"
-    (is (= 'foo @(now 'foo)))))
+    (is (= 'foo @(task/now 'foo)))))
 
 (deftest run-test
   (testing "run works"
-    (is (= 123 @(run 123)))))
+    (is (= 123 @(task/run 123)))))
 
 (deftest then-tests
   (testing "basic application"
-    (is (= 2 @(then inc (now 1)))))
+    (is (= 2 @(task/then inc (now 1)))))
 
   (testing "comp works"
-    (is (= 3 @(then (comp inc inc) (now 1))))))
+    (is (= 3 @(task/then (comp inc inc) (now 1))))))
 
 (deftest compose-tests
   (testing "basic application"
-    (is (= 2 @(compose #(run (inc %)) (now 1))))))
+    (is (= 2 @(task/compose #(task/run (inc %)) (now 1))))))
 
 (deftest for-tests
   (testing "for works"
-    (is (= 11 @(for [a (future 1)
-                     b (now 3)
-                     c (run 7)]
+    (is (= 11 @(task/for [a (future 1)
+                     b (task/now 3)
+                     c (task/run 7)]
                  (+ a b c))))))
 
 (deftest sequence-tests
   (testing "sequence works"
-    (is (= [1 2 3] @(sequence [(run 1) (now 2) (future 3)])))))
+    (is (= [1 2 3] @(task/sequence [(task/run 1) (now 2) (future 3)])))))
 
 (deftest complete-tests
   (testing "complete! completes"
     (let [task (run (Thread/sleep 2000) 9)]
-      (complete! task "bla")
+      (task/complete! task "bla")
       (is (= "bla" @task))))
   (testing "complete! doesn't override a ready task"
-    (let [task (now 1)]
-      (complete! task "fff")
+    (let [task (task/now 1)]
+      (task/complete! task "fff")
       (is (= 1 @task))))
   (testing "force! forces a value"
-    (let [task (now 'foo)]
-      (force! task 'bar)
+    (let [task (task/now 'foo)]
+      (task/force! task 'bar)
       (is (= 'bar @task)))))
 
 (deftest failure-tests
   (testing "get on nonsense throws ExecutionException "
-    (is (thrown-with-msg? ExecutionException #"ArithmeticException" @(run (/ 1 0)))))
+    (is (thrown-with-msg? ExecutionException #"ArithmeticException" @(task/run (/ 1 0)))))
   (testing "failed? works"
-    (is (failed? (failed (IllegalStateException. "asdf")))))
+    (is (task/failed? (task/failed (IllegalStateException. "asdf")))))
   (testing "failure works"
-    (is (instance? RuntimeException (failure (failed (RuntimeException. "foo")))))
-    (is (= nil (failure (now 1)))))
+    (is (instance? RuntimeException (task/failure (task/failed (RuntimeException. "foo")))))
+    (is (= nil (task/failure (now 1)))))
   (testing "fail! works"
-    (let [ff (failed (RuntimeException. "bork"))]
-      (fail! ff (IllegalStateException. "bad state"))
-      (is (instance? IllegalStateException (failure ff))))))
+    (let [ff (task/failed (RuntimeException. "bork"))]
+      (task/fail! ff (IllegalStateException. "bad state"))
+      (is (instance? IllegalStateException (task/failure ff))))))
 
 (deftest misc-tests
   (testing "now is always done"
-    (is (done? (now 1))))
+    (is (task/done? (task/now 1))))
   (testing "void is void"
-    (is (not (done? (void)))))
+    (is (not (task/done? (task/void)))))
   (testing "else works"
-    (is (= 'foo (else (void) 'foo)))
-    (is (= 'bar (else (run (Thread/sleep 1000) 'baz) 'bar)))))
+    (is (= 'foo (task/else (task/void) 'foo)))
+    (is (= 'bar (task/else (task/run (Thread/sleep 1000) 'baz) 'bar)))))
 
 (deftest recover-tests
   (testing "recover works"
     (is (= 'bla
-           @(recover (failed (IllegalArgumentException.))
-                     ; let's be picky...
-                     (fn [x] (when (= IllegalArgumentException (class x))
-                               'bla)))))))
+           @(task/recover (task/failed (IllegalArgumentException.)) ; let's be picky...
+                          (fn [x] (when (= IllegalArgumentException (class x))
+                                    'bla)))))))
