e4cb6b3f7ac65a757b209f09de8619a547d08510
Antoine Kalmbach <ane@iki.fi>
2017-12-03 20:57:08 +0200

    Add stuff

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.ane/task "0.2.0"
+(defproject com.github.ane/task "0.2.0-SNAPSHOT"
   :description "Task is a Clojure library for asynchronous
   computation. It provides functional primitives that
   makes concurrent programming accessible and powerful."
@@ -6,11 +6,11 @@
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.github.ane/codox-kingfisher-theme "0.1.0-SNAPSHOT"]]
+                 [com.github.ane/codox-kingfisher-theme "0.1.0"]]
   :source-paths ["src/main/clj"]
   :test-paths ["src/test/clj"]
   :deploy-repositories [["releases" :clojars]]
-  :resource-pahts ["src/main/resources"]
+  :resource-paths ["src/main/resources"]
   :plugins [[test2junit "1.1.2"]
             [lein-codox "0.10.3"]
             [lein-exec "0.3.7"]]
